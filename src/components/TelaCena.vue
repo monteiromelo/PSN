6421e664cb9f037c85065832b0ca89b68747b957
david macedo <david@alu.ufc.br>
2020-04-21 23:12:44 -0300

    first commit

---
+++ src/components/TelaCena.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <button @click="importaImagem"> 
+        Oie 
+        </button>
+        <img :src="img">
+        <!-- <img src="../assets/gaga.png"> -->
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+        img: '../assets/gaga.png',
+        }
+    },
+    methods:{
+        importaImagem(){
+
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
